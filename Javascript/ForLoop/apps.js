b6616c1fdc87c7de67be8d2a08f9d9609c565421
Lokman Bin Khodziri <45467762+LokmanKhodziri@users.noreply.github.com>
2020-08-04 21:01:16 +0800

    Add files via upload

---
+++ Javascript/ForLoop/apps.js
@@ -0,0 +1,12 @@
+// var colors = ["red", "orange", "yellow", "green"];
+
+// for(var i =0; i < colors.length; i++ ){
+//     console.log(colors[i]);
+// }
+
+//for earch loop
+ var colors = [ "red", "orange", "yellow", "green"];
+
+ colors.forEach(function(colors){
+    alert(colors);
+});
---
+++ Javascript/TodoNew/TodoNEW.js
@@ -0,0 +1,17 @@
+var todos = ["Buy New Turtle"];
+
+var input = prompt("What would you like to do?");
+
+while(input !== "quit"){
+    if(input === "list"){
+        todos.forEach(function(todo){
+            console.log(todo);
+        });
+    }else if(input === "new"){
+        var newTodo = prompt("Enter new todo");
+        todos.push(newTodo);
+    }
+    input = prompt("What would you like to do?");
+}
+
+console.log("OK, YOU QUIT THE APP");
